2486f8b166486f44210922163b102c5e8dbdc378
chaohg <chaohg@hotmail.com>
2021-05-12 22:11:33 -0700

    reactnative- add about and contact components

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import { render } from 'react-dom';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
