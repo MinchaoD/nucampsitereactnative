a83ba3021387336b81f9bc6af4bf7d615583bb3a
chaohg <chaohg@hotmail.com>
2021-05-16 00:33:37 -0700

    add icons and custom side drawer

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,14 +4,23 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
-import { View, Platform } from 'react-native';
+import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+import SafeAreaView from 'react-native-safe-area-view';
 
 const DirectoryNavigator = createStackNavigator(
     {
-        Directory: { screen: Directory },
+        Directory: { screen: Directory,
+                     navigationOptions: ({navigation}) => ({ //this is to add icon to the text of directory on the left
+                         headerLeft: <Icon
+                            name='list'
+                            type="font-awesome"
+                            iconStyle={styles.stackIcon }
+                            onPress={() => navigation.toggleDrawer()} />  //this toggleDrawer is the built in to the navigator, it is to open the drawer navigator if press the icon
+                     }) },
         CampsiteInfo: { screen: CampsiteInfo }  //these 2 are the components will be available for stacknavigator
     },
     {
@@ -33,15 +42,23 @@ const HomeNavigator = createStackNavigator(
         Home: { screen: Home }
     },
     {
-        defaultNavigationOptions: {
+        defaultNavigationOptions: ({navigation}) => ({  //the newly added Icon code can be added here 
+            //and pass navigation instead of like the above directory, which need to under directory because 
+            // there is another campsiteInfo
+        
             headerStyle: {
                 backgroundColor: '#5637DD'
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
-            }
-        }
+            },
+            headerLeft: <Icon
+                name='home'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()} />
+        })
     }
 );
 
@@ -50,15 +67,20 @@ const AboutNavigator = createStackNavigator(
         About: { screen: About }
     },
     {
-        defaultNavigationOptions: {
+        defaultNavigationOptions: ({navigation}) => ({
             headerStyle: {
                 backgroundColor: '#5637DD'
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
-            }
-        }
+            },
+            headerLeft: <Icon
+                name='info-circle'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}/>
+        })
     }
 );
 
@@ -67,27 +89,96 @@ const ContactNavigator = createStackNavigator(
         Contact: { screen: Contact }
     },
     {
-        defaultNavigationOptions: {
+        defaultNavigationOptions: ({navigation}) => ({
             headerStyle: {
                 backgroundColor: '#5637DD'
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
-            }
-        }
+            },
+            headerLeft: <Icon
+                name='address-card'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()} />
+        })
     }
 );
 
+const CustomDrawerContentComponent = props => (
+    <ScrollView>
+        <SafeAreaView  // these are the code for set up safe area for phones
+            style={styles.container}
+            forceInset={{top: 'always', horizontal: 'never'}}>
+            {/* below is set image to be 1/3, and text to be 2/3, horizontally, because below style.drawerHeader has flexdirection set to row*/}
+            <View style={styles.drawerHeader}>
+                <View style={{flex: 1}}>  
+                    <Image source = {require('./images/logo.png')} style={styles.drawerImage} />
+                </View>
+                <View style={{flex:2}}> 
+                    <Text style={styles.drawerHeaderText}>NuCamp</Text>
+                </View>
+            </View>
+            <DrawerItems {...props} /> 
+            {/* the above code is to render all the other components, like home, directory, etc, they are passed as props here. DrawerItems is imported from library. */}
+        </SafeAreaView>
+    </ScrollView> 
+
+)
+
+
 const MainNavigator = createDrawerNavigator(
     {
-        Home: { screen: HomeNavigator },
-        Directory: { screen: DirectoryNavigator },
-        About: { screen: AboutNavigator},
-        Contact: { screen: ContactNavigator},
+        Home: { screen: HomeNavigator,
+                navigationOptions: {
+                    drawerIcon: ({tintColor}) => ( // here drawerIcon and tintColor are all built in, can not be anyname,
+                        //tintColor is for when it is current, it will be blue, otherwise will be tinted as grey,
+                        // for the screen name like Home, Directory.. those tintColor features are built in, but for icons, we need to write tintColor
+                        <Icon
+                            name='home'
+                            type='font-awesome'
+                            size={24}  //you can also write size and color in here instead of in stylesheet below
+                            color={tintColor} />
+                    )
+
+                } },
+        Directory: { screen: DirectoryNavigator,
+                     navigationOptions: {
+                         drawerIcon: ({tintColor}) => (
+                             <Icon
+                                name='list'
+                                type='font-awesome'
+                                size={24}
+                                color={tintColor} />
+                         )
+                     } },
+        About: { screen: AboutNavigator,
+                 navigationOptions: {
+                     drawerLabel: 'About Us', //this is to overwrite the text of this drawer navigation, it was About, now change to About Us
+                     drawerIcon: ({tintColor}) => (
+                         <Icon
+                            name='info-circle'
+                            type='font-awesome'
+                            size={24}
+                            color={tintColor} />
+                     )           
+                    }},
+        Contact: { screen: ContactNavigator,
+                   navigationOptions: {
+                       drawerLabel: 'Contact Us',
+                       drawerIcon: ({tintColor}) => (
+                           <Icon
+                                name='address-card'
+                                type='font-awesome'
+                                size={24}
+                                color={tintColor} />
+                       )
+                   }},
     },
     {
-        drawerBackgroundColor: '#CEC8FF'
+        drawerBackgroundColor: '#CEC8FF',
+        contentComponent: CustomDrawerContentComponent  //this is add the custom drawer to this content, to replace the old one
     }
 )
 
@@ -104,5 +195,34 @@ class Main extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    stackIcon: {
+        marginLeft: 10,
+        color: '#fff',
+        fontSize:24
+    },
+    container: {
+        flex: 1,
+    },
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row' 
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
+    }
+})
 
 export default Main;
