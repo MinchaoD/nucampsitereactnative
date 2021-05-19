ce9702abc1f9a41ee48ac66a28d9f5116010f81d
chaohg <chaohg@hotmail.com>
2021-05-18 23:28:16 -0700

    add search  modal

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 class Reservation extends Component {
@@ -9,7 +9,8 @@ class Reservation extends Component {
             campers: 1,
             hikeIn: false,
             date: new Date(),
-            showCalendar: false
+            showCalendar: false,
+            showModal: false
         }
     }
 
@@ -17,13 +18,22 @@ class Reservation extends Component {
         title: 'Reserve Campsite'
     }
 
+    toggleModal() {
+        this.setState({showModal: !this.state.showModal})
+    }
+
     handleReservation () {
         console.log(JSON.stringify(this.state));
+        this.toggleModal();
+    }
+
+    resetForm() {
         this.setState({  //after the handlreservation, set the state back to original data
             campers:1,
             hikeIn: false,
             date: new Date(),
-            showCalendar: false
+            showCalendar: false,
+            showModal: false
         })
     }
     render() {
@@ -75,10 +85,28 @@ class Reservation extends Component {
                         onPress={() => this.handleReservation()}
                         title='Search'
                         color="#5637DD"
-                        color='#5637DD'
                         accessibilityLabel='Tap me to select a reservation date'
                     />
                 </View>
+                <Modal 
+                    animationType= {'slide'}  //set the modal slide from bottom to up
+                    transparent={false}
+                    visible={this.state.showModal}
+                    onRequestClose={() => this.toggleModal()}>
+                    <View style={styles.modal}>
+                        <Text style={styles.modalTitle}>Search</Text>
+                        <Text style={styles.modalText}>Number of Campers: {this.state.campers}</Text>
+                        <Text style={styles.modalText}>Hike-In? : {this.state.hikeIn ? 'Yes' : 'No'}</Text>
+                        <Text style={styles.modalText}>Date: {this.state.date.toLocaleDateString('en-US')}</Text>
+                        <Button 
+                            onPress = {() => {
+                                this.toggleModal();
+                                this.resetForm();
+                            }}
+                            color = '#5637DD'
+                            title = 'Close' />
+                    </View>
+                </Modal>
             </ScrollView>
         )
     }
@@ -98,6 +126,22 @@ const styles = StyleSheet.create({
     },
     formItem: {
         flex: 1
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    },
+    modalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        backgroundColor:'#5637DD',
+        textAlign: 'center',
+        color: '#fff',
+        marginBottom: 20
+    },
+    modalText: {
+        fontSize: 18,
+        margin: 10
     }
 });
 
